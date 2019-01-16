c14f1aafaaba15227d5352f5cf95182e13ce8709
Amri <amrikarisma@yahoo.com>
2019-01-17 02:45:30 +0700

    static html base react js

---
+++ react-client/src/Components/HeaderComponent/NavBar.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+class NavBar extends Component {
+  render() {
+    return (
+      <header>
+     {/* Start Navbar */}
+     <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link to="/" className="navbar-brand">Horeka</Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <div className="input-group input-style">
+              <div className="input-group-prepend">
+                <button className="btn dropdown-toggle dropdown-style" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All</button>
+                <div className="dropdown-menu">
+                  <a className="dropdown-item" href="#">item</a>
+                  <a className="dropdown-item" href="#">item</a>
+                  <a className="dropdown-item" href="#">item</a>
+                  <div role="separator" className="dropdown-divider" />
+                  <a className="dropdown-item" href="#">item</a>
+                  <a className="dropdown-item" href="#">item</a>
+                  <a className="dropdown-item" href="#">item</a>
+                </div>
+              </div>
+              <input type="search" className="form-control form-style" aria-label="search item" placeholder="what are you looking for...." />
+              <div className="input-group-append">
+                <button className="btn btn-group-text btn-group-style bg-main text-white" type="button" id="button-addon2">SEARCH</button>
+              </div>
+            </div>
+            <div className="navbar-nav">
+              <a className="nav-item nav-link" href="#"><i className="fa fa-list-alt fa-2x" /><span className="align-top text-style">DAILY MARKET LIST</span></a>
+              <a className="nav-item nav-link" href="#"><i className="fa fa-search fa-2x" aria-hidden="true" /></a>
+              <a className="nav-item nav-link" href="#"><i className="fa fa-shopping-cart fa-2x" aria-hidden="true" /></a>
+            </div>
+            <div className="btn-group" role="group" aria-label="Basic example">
+              <button type="button" className="btn btn-register text-center">REGISTER</button>
+              <button type="button" className="btn btn-login text-center">LOGIN</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+      {/* End Navbar */}
+      </header>
+    )
+  }
+}
+export default NavBar;
---
+++ react-client/src/Components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        Home page test
+      </div>
+    )
+  }
+}
+export default HomePage;
