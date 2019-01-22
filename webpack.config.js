1a01d2d13fc2110433b9e35d61aea354e4158642
Albert Panesse <albert.panesse@gmail.com>
2019-01-22 17:19:45 +0700

    object spread plugin

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,10 @@ module.exports = {
         include : SRC_DIR,
         loader : 'babel-loader',      
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015'],
+          plugins: [
+            ["transform-object-rest-spread"]
+          ]
        }
       }
     ]
